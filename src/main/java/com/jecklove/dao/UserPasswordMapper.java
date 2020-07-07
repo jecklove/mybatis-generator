9e323964d18f9930303cdbf9ad3d5a5b29db101e
jecklove <m18855066040@126.com>
2020-07-07 15:44:40 +0800

    Initial commit

---
+++ src/main/java/com/jecklove/dao/UserPasswordMapper.java
@@ -0,0 +1,17 @@
+package com.jecklove.dao;
+
+import com.jecklove.entity.UserPassword;
+
+public interface UserPasswordMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserPassword record);
+
+    int insertSelective(UserPassword record);
+
+    UserPassword selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserPassword record);
+
+    int updateByPrimaryKey(UserPassword record);
+}
